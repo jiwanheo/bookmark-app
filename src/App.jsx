eafb36d6f32a17206bbb2d306576d6f1a718f79b
Jiwan Heo <jiwanheo123@gmail.com>
2024-04-03 18:12:36 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [bookmarkUrl, setBookmarkUrl] = useState();
+
+  const handleSubmit = function() {
+    alert(`A new url was submitted: ${bookmarkUrl}`);
+  }
+
+  const handleChange = function(event) {
+    setBookmarkUrl(event.target.value);
+  }
+
+  return (
+    <>
+      <h1>My bookmarking app</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Enter URL:
+          <input type="text" name="url-text" onChange={handleChange}/>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </>
+  )
+}
+
+export default App
